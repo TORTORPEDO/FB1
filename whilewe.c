c7914240a2d25d442e54c5874df729aa00f715f8
Sid P <155798013+TORTORPEDO@users.noreply.github.com>
2024-01-29 19:39:48 -0500

    Add files via upload

---
+++ whilewe.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+
+int main (void ) {
+
+  /* void function*/
+
+
+      /* integer declaration */
+
+       int i = 4; 
+       int j = 3;
+       int r;
+
+       int up = 200;
+
+
+
+  /* while statement to print new function */
+    /* while 4 is less than 0
+      update i as + 200 and r is equal to
+      9 * 4 */
+
+    while (i <= j) { 
+        r = (9) * (i * j);
+            i = i + up;
+            } printf("%d", r);
+}
